c9dec51a26c5d58f91ee927f47c5aff09e92b2e6
helloworldforeverlove <134216852+helloworldforeverlove@users.noreply.github.com>
2024-07-14 17:48:22 +0000

    dddddddddddds

--- src/pages/12ValeurPatrimoineImmobilierNet.tsx
+++ src/pages/12ValeurPatrimoineImmobilierNet.tsx
@@ -72,10 +72,15 @@ const ValeurPatrimoineImmobilierNet: React.FC = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const formatNumberWithSpaces = (num: number): string => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    };
+
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(event.target.value, 10);
-        if (!isNaN(value) && value >= 0) {
-            setValue(value);
+        const value = event.target.value.replace(/\s+/g, '');
+        const parsedValue = parseInt(value, 10);
+        if (!isNaN(parsedValue) && parsedValue >= 0) {
+            setValue(parsedValue);
             setIsInvalidInput(false);
         } else {
             setValue(null);
@@ -105,9 +110,9 @@ const ValeurPatrimoineImmobilierNet: React.FC = () => {
                 <Box justifyContent="center" mb={6} maxWidth={400} mx="auto">
                     <InputGroup size="lg" width="auto">
                         <Input
-                            type="number"
+                            type="text"
                             min={0}
-                            value={value !== null ? value : ''}
+                            value={value !== null ? formatNumberWithSpaces(value) : ''}
                             onChange={handleInputChange}
                             placeholder="Entrez une valeur"
                             size="lg"
@@ -140,7 +145,7 @@ const ValeurPatrimoineImmobilierNet: React.FC = () => {
                 {value !== null && (
                     <Box borderWidth="1px" borderRadius="md" p={4} mt={4} textAlign="center" borderColor="yellow.400">
                         <Text fontSize="2xl" color="yellow.500">
-                            {value} €
+                            {formatNumberWithSpaces(value)} €
                         </Text>
                     </Box>
                 )}
